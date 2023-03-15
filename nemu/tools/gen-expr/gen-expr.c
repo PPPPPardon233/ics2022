97f14c0905bbd2ad3cbc5bbfdd7382ba9a3f1b69
tracer-ics2022 <tracer@njuics.org>
2023-03-15 16:53:46 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:46 up 32 min,  1 user,  load average: 0.40, 0.39, 0.42

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -51,7 +51,7 @@ static void gen_rand_expr() {
     case 0:{
       sprintf(expr_,"%d",rand()%MAX);
       strcat(buf,expr_);
-      strcat(buf,"U");
+      //strcat(buf,"U");
       break;
     }
     case 1:{
