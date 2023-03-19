fed1c24bb3e1c489ab742995da441c4d99156e75
tracer-ics2022 <tracer@njuics.org>
2023-03-19 23:09:57 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:09:56 up 13:42,  1 user,  load average: 0.55, 0.45, 0.37

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -16,6 +16,10 @@
 #include <isa.h>
 #include "local-include/reg.h"
 
+#define NR_REGS ARRLEN(regs) //sizeof(regs)/sizeof(regs[0])
+#define NR_CSRS ARRLEN(csrs) //sizeof(csrs)/sizeof(csrs[0])
+
+
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -23,7 +27,19 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
+const char *csrs[] = {
+  "mtvec","mepc","mstatus","mcause"
+};
+
 void isa_reg_display() {
+  printf("%-15s%-17s%-15s\n", "Registers", "Hexadecimal", "Decimal");
+  for(int i = 0; i < NR_REGS; ++i){
+    printf("%-15s0x%-15x%-15d\n",regs[i], cpu.gpr[i], cpu.gpr[i]);
+  }
+  printf("%-15s0x%-15x%-15d\n","pc", cpu.pc, cpu.pc);
+  // for(int i = 0; i < NR_CSRS; ++i){
+  //   printf("%-15s0x%-15x%-15d\n",csrs[i], cpu.csr[i], cpu.csr[i]);
+  // }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -16,9 +16,6 @@
 #include <isa.h>
 #include "local-include/reg.h"
 
-#define NR_REGS ARRLEN(regs) //sizeof(regs)/sizeof(regs[0])
-#define NR_CSRS ARRLEN(csrs) //sizeof(csrs)/sizeof(csrs[0])
-
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -26,19 +23,7 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-const char *csrs[] = {
-  "mtvec","mepc","mstatus","mcause"
-};
-
 void isa_reg_display() {
-  printf("%-15s%-17s%-15s\n", "Registers", "Hexadecimal", "Decimal");
-  for(int i = 0; i < NR_REGS; ++i){
-    printf("%-15s0x%-15x%-15d\n",regs[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
-  }
-  printf("%-15s0x%-15x%-15d\n","pc", cpu.pc, cpu.pc);
-  for(int i = 0; i < NR_CSRS; ++i){
-    printf("%-15s0x%-15x%-15d\n",csrs[i], cpu.csr[i]._32, cpu.csr[i]._32);
-  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
