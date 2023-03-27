5b4354007d8c5fc8ff6fa65f7287e84e72bed00a
tracer-ics2022 <tracer@njuics.org>
2023-03-27 14:53:25 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:25 up 1 day,  5:08,  1 user,  load average: 0.18, 0.41, 0.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,42 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char* get_int(char *p, va_list *ap) {
+	int d = va_arg(*ap, int);
+	char str[32];
+	int len = 0;
+	if (d == 0) {
+		*p++ = '0';
+		return p;
+	}
+	if (d < 0) {
+		*p++ = '-';
+		d *= -1;
+	}
+	while (d) {
+		str[len++] = d % 10 + '0';
+		d /= 10;
+	}
+	for (int i = len-1; i >= 0; i--) {
+		*p++ = str[i];
+	}
+	return p;
+}
+
+static char* get_string(char* p, va_list *ap) {
+	char *str = va_arg(*ap, char*);
+	while(*str) {
+		*p++ = *str++;
+	}
+	return p;
+}
+
+static char* get_char(char* p, va_list *ap) {
+	char ch = (char)va_arg(*ap, int);
+	*p++ = ch;
+	return p;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +50,27 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  va_list ap;
+	va_start(ap, fmt);
+  char* p = (char*)out;
+	while (*fmt) {
+		if (*fmt == '%') {
+			fmt++;
+			switch (*fmt) {
+				case 'd': p = get_int(p, &ap);break;
+				case 's': p = get_string(p, &ap);break;
+				case 'c': p = get_char(p, &ap);break;
+			}
+			fmt++;
+		}
+		else {
+			*p++ = *fmt++;
+		}
+	}
+	*p++ = '\0';
+	va_end(ap);
+	return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,122 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t len = 0;
+  char *t = (char*) s;
+	while (*t++ != '\0') {
+		len ++;
+	}
+	return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i;
+	for (i = 0; src[i] != '\0'; i++)
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i = 0;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dst[i] = src[i];
+	for (; i < n; i++)
+		dst[i] = '\0';
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i = 0;
+	size_t len = strlen(dst);
+	for (i = 0; src[i] != '\0'; i++)
+		dst[len+i] = src[i];
+	dst[len+i] = '\0';
+	return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  int flag = 1;
+	while(*s2) {
+		flag = *s1 - *s2;
+		if(flag) {
+			break;
+		}
+		++s1;
+		++s2;
+	}
+	return flag;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  int flag = 1;
+	while(n-- && *s2) {
+		flag = *s1 - *s2;
+		if (flag) {
+			break;
+		}
+		++s1;
+		++s2;
+	}
+	return flag;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  size_t i = 0;
+	char *t = (char*) s;
+	for (i = 0; i < n; i++)
+		t[i] = (char)c;
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char *p = (char*)dst;
+	char *q = (char*)src;
+	if (src >= dst) {
+		while (n--) {
+			*p++ = *q++;
+		}
+	}
+	else {
+		p += n;
+		q += n;
+		while (n--) {
+			*p-- = *q--;
+		}
+	}
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char* p = (char*)out;
+	char* q = (char*)in;
+	while (n--) {
+		*p++ = *q++;
+	}
+	return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  //panic("Not implemented");
+  char *p = (char*)s1;
+	char *q = (char*)s2;
+	int flag = 0;
+	while (n--) {
+		flag = *p - *q;
+		if (!flag) break;
+		++p;
+		++q;
+	}
+	return flag;
 }
 
 #endif
