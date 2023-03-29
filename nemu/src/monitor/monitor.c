680f0d1b380b5f4097efb6ca416d1c92c2da075e
tracer-ics2022 <tracer@njuics.org>
2023-03-29 22:00:25 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:00:25 up 2 days,  9:12,  1 user,  load average: 0.89, 0.65, 0.62

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -86,7 +86,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'e': elf_file = optarg; break;
+      case 'e': elf_file = optarg; printf("hhh");break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
