ed86e64f174a01aa918abe107eda5a6126d707bf
tracer-ics2022 <tracer@njuics.org>
2023-03-29 21:24:31 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:31 up 2 days,  8:36,  1 user,  load average: 0.87, 0.84, 0.77

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -94,6 +94,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=ELF            analyze elf file\n");
         printf("\n");
         exit(0);
     }
