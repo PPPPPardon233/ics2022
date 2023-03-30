932c785083d4c6cc4b6dcec1d42c4e59676a6754
tracer-ics2022 <tracer@njuics.org>
2023-03-30 09:58:26 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:58:26 up 2 days, 21:01,  1 user,  load average: 0.91, 0.74, 0.64

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -80,7 +80,7 @@ static int parse_args(int argc, char *argv[]) {
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
