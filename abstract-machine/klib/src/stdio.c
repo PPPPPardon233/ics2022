7593121088eb30877687b8cbe9803b0ed85e33ae
tracer-ics2022 <tracer@njuics.org>
2023-04-06 10:54:57 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:54:57 up 10:57,  1 user,  load average: 0.54, 0.79, 0.81

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -116,7 +116,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 						append(buffer[k]);
 					break;  
 				default:
-					assert(0);
+					//assert(0);
 			}
 		state = 0;
 		break;
