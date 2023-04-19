9867531ed9eaad50d65b4233e25f1667deb6bca5
tracer-ics2022 <tracer@njuics.org>
2023-04-19 20:55:04 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:04 up  9:21,  1 user,  load average: 0.69, 0.57, 0.48

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -141,7 +141,7 @@ static int decode_exec(Decode *s) {
 	
 	INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 	INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
-	INSTPAT("0011000 00010 00000 000 00000 11100 11", mret	 , N, s->dnpc = cpu.csr.mepc );
+	INSTPAT("0011000 00010 00000 000 00000 11100 11", mret	 , N, s->dnpc = cpu.csr.mepc);
   INSTPAT_END(); 
 
   R(0) = 0; // reset $zero to 0
