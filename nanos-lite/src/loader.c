499b39400218719576eaeaa6a86e037949ad22ca
tracer-ics2022 <tracer@njuics.org>
2023-04-20 16:42:05 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:05 up 12:57,  1 user,  load average: 0.24, 0.28, 0.34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -62,7 +62,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
   for (int i = 0; i < ehdr.e_phnum; i++) {
     if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_write((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
       memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
