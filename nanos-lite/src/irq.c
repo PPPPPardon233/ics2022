21c665ab9f6a568153c096fd98d3871403640e51
tracer-ics2022 <tracer@njuics.org>
2023-04-20 17:27:01 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:27:01 up 13:42,  1 user,  load average: 0.78, 0.52, 0.44

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,14 @@
 #include <common.h>
 
+extern void do_syscall(Context *c);
+
 static Context* do_event(Event e, Context* c) {
   if (e.event == EVENT_YIELD ) {
     printf("EVENT_YIELD, event ID %d\n", e.event);
   }
   else if (e.event == EVENT_SYSCALL ) {
     printf("EVENT_SYSCALL, do_event ID %d, count %d", e.event, c->GPR4); 
+    do_syscall(c);
   }
   else {
     panic("PANIC event ID %d", e.event);
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,104 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
+#include <fs.h>
+#include <proc.h>
+#include <sys/time.h>
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
+int sys_yield()
+{
+	yield();
+	return 0;
+}
+
+int sys_exit()
+{
+	halt(0);
+	return 0;
+}
+
+int sys_write(int fd, const void *buf, size_t count)
+{
+	return fs_write(fd, buf, count);
+}
+
+int sys_gettimeofday(Context *ctx)
+{
+  struct timeval *tv = (struct timeval *)ctx->GPR2;
+  __uint64_t time = io_read(AM_TIMER_UPTIME).us;
+  tv->tv_usec = (time % 1000000);
+  tv->tv_sec = (time / 1000000);
+  ctx->GPRx = 0;
+	return 0;
+}
+
+int sys_open(Context *c) {
+  int ret = fs_open((char *)c->GPR2, c->GPR3, c->GPR4);
+  return ret;
+}
+
+int sys_read(Context *c){
+  return fs_read(c->GPR2, (void *)c->GPR3, c->GPR4);
+}
+
+int sys_close(Context *c){
+  return fs_close(c->GPR2);
+}
+
+int sys_lseek(Context *c){
+  return fs_lseek(c->GPR2, c->GPR3, c->GPR4);
+}
+
+void do_syscall(Context *ctx) {
+  Log("strace", ctx->mcause, ctx->GPR1, ctx->GPR2, ctx->GPR3, ctx->GPR4);
+
+	uintptr_t a[4];
+	a[0] = ctx->GPR1;
+	a[1] = ctx->GPR2;
+	a[2] = ctx->GPR3;
+	a[3] = ctx->GPR4;
+
+  if ( a[0] == SYS_exit) {
+    ctx->GPRx = sys_exit();
+  } else if ( a[0] == SYS_yield) {
+    ctx->GPRx = sys_yield();
+  } else if ( a[0] == SYS_write )  {
+    ctx->GPRx = sys_write(a[1], (void *)a[2], a[3]);
+  } else if ( a[0] == SYS_open ) {
+    ctx->GPRx = sys_open(ctx);
+  } else if ( a[0] == SYS_read) {
+    ctx->GPRx = sys_read(ctx);
+  } else if ( a[0] == SYS_close) {
+    ctx->GPRx = sys_close(ctx);
+  } else if ( a[0] == SYS_lseek) {
+    ctx->GPRx = sys_lseek(ctx);
+  } else if ( a[0] == SYS_gettimeofday) {
+    ctx->GPRx = sys_gettimeofday(ctx);
+  } else if ( a[0] == SYS_brk) {
+    ctx->GPRx = 0;
+  } else {
+    panic("Unhandled syscall ID = %d", a[0]);
   }
 }
