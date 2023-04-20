7595855488582b5c659fbd699a96a2bd4e330ef1
tracer-ics2022 <tracer@njuics.org>
2023-04-20 15:01:02 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:01:02 up 11:16,  1 user,  load average: 0.27, 0.33, 0.42

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -7,6 +7,11 @@
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
+void see_if_this_is_loaded(){
+  return;
+}
+
 int main() {
+  see_if_this_is_loaded();
   return _syscall_(SYS_yield, 0, 0, 0);
 }
