2929547d2d30db68769532e3ed5f1cff631cc9d6
tracer-ics2022 <tracer@njuics.org>
2023-04-20 20:16:54 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:54 up 16:32,  1 user,  load average: 0.92, 0.56, 0.45

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,12 +5,9 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
+  while (j!=100000) {
+    printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+    j++;
   }
   return 0;
 }
