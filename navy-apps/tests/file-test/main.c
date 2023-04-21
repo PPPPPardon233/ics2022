9248cd95e58805c47b85352940700b0e9336e0ab
tracer-ics2022 <tracer@njuics.org>
2023-04-21 15:34:04 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:04 up 23:18,  1 user,  load average: 0.50, 0.72, 0.65

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,6 +2,13 @@
 #include <assert.h>
 
 int main() {
+  write(1, "Hello World!\n", 13);
+  int i = 2;
+  volatile int j = 0;
+  while (j!=2) {
+    printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+    j++;
+  }
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
   printf("Hello World from file-test!\n");
