27a10c99902d8d0198c1c76bceb46b01ae7ff836
tracer-ics2022 <tracer@njuics.org>
2023-04-21 14:52:28 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:52:28 up 22:36,  1 user,  load average: 0.33, 0.41, 0.42

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -54,14 +54,14 @@ size_t fs_read(int fd, void *buf, size_t len){
 	Finfo *info = &file_table[fd];
   size_t real_len;
 
-  // if (info->read){
-  //   real_len = info->read(buf, info->open_offset, len);
-  // }
-  // else {
+  if (info->read){
+    real_len = info->read(buf, info->open_offset, len);
+  }
+  else {
     real_len = info->open_offset + len <= info->size ?
     len : info->size - info->open_offset;
     ramdisk_read(buf, info->disk_offset + info->open_offset, real_len);
-  //}
+  }
   info->open_offset += real_len;
   return real_len;
 }
