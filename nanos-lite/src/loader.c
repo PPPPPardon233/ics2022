9cfa1a4fa9d17bdeafdaabc61f9bc89e8b56d1ee
tracer-ics2022 <tracer@njuics.org>
2023-04-21 14:59:15 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:15 up 22:43,  1 user,  load average: 0.93, 0.61, 0.49

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,7 +45,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   * Check whether the elf-file meets the specification 
   */
 	fs_read(fd, &header, sizeof(Elf32_Ehdr));
-	if (*(uint32_t *)header.e_ident == (uint32_t)0x464C457F){
+	if (*(uint32_t *)header.e_ident == (uint32_t)0X464C457F){
 		Log("header.e_ident=%x,it should be 0x464c457f",*(uint32_t *)header.e_ident );
     assert(0);
 	}
