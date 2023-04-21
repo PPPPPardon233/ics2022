6983dad6522e24b2a972d229789e72303ce43f71
tracer-ics2022 <tracer@njuics.org>
2023-04-21 15:28:58 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:58 up 23:13,  1 user,  load average: 0.97, 0.69, 0.61

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -18,7 +18,7 @@ int main() {
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
-    fprintf(fp, "%4d\n", i + 1 + 1000);
+    //fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
   for (i = 500; i < 1000; i ++) {
