92623f87b7fc784d3a750630f1e79aef7f002a19
tracer-ics2022 <tracer@njuics.org>
2023-04-21 10:14:02 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:14:02 up 17:58,  1 user,  load average: 0.56, 0.56, 0.56

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   if (e.event == EVENT_YIELD ) {
     printf("EVENT_YIELD, event ID %d\n", e.event);
-    do_syscall(c);
+    //do_syscall(c);
   }
   else if (e.event == EVENT_SYSCALL ) {
     printf("EVENT_SYSCALL, do_event ID %d, count %d\n", e.event, c->GPR4); 
