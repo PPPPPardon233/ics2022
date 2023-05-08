171a4bfbf88d2c81408bf56418f3fb88270c1806
tracer-ics2022 <tracer@njuics.org>
2023-05-08 21:56:21 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:21 up  4:27,  1 user,  load average: 0.92, 0.67, 0.62

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -62,8 +62,8 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     w = disp_size.w;
     h = disp_size.h;
   }
-  assert(w > 0 && w <= disp_size.w);
-  assert(h > 0 && h <= disp_size.h);
+  //assert(w > 0 && w <= disp_size.w);
+  //assert(h > 0 && h <= disp_size.h);
 
   // write(1, "here\n", 10);
   // printf("draw [%d, %d] to [%d, %d]\n", w, h, x, y);
