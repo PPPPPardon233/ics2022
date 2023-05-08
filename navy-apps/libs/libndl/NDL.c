4b4b798b9b4fd14fcdb789878a3a27a56d4e482f
tracer-ics2022 <tracer@njuics.org>
2023-05-08 22:02:34 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:34 up  4:33,  1 user,  load average: 1.01, 0.75, 0.66

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -98,7 +98,7 @@ int NDL_Init(uint32_t flags) {
   // get_disp_size();
   FILE *fp = fopen("/proc/dispinfo", "r");
   fscanf(fp, "WIDTH:%d\nHEIGHT:%d\n", &disp_size.w, &disp_size.h);
-  // printf("disp size is %d,%d\n", disp_size.w, disp_size.h);
+  printf("disp size is %d,%d\n", disp_size.w, disp_size.h);
   assert(disp_size.w >= 400 && disp_size.w <= 800);
   assert(disp_size.h >= 300 && disp_size.h <= 640);
   fclose(fp);
