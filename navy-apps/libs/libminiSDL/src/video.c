bdb4f5ebad049bd23c3d958ae2d18ceaa341b8f3
tracer-ics2022 <tracer@njuics.org>
2023-05-08 22:40:18 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:40:18 up  5:11,  1 user,  load average: 0.73, 4.44, 3.33

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -12,37 +12,74 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
-void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h)
-{
-  // improtant: only w == 0 && h == 0, then set the s size.
-  if (w == 0 && h == 0){
-    w = s->w;
-    h = s->h;
-  }
+void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h){
+  // // improtant: only w == 0 && h == 0, then set the s size.
+  // if (w == 0 && h == 0){
+  //   w = s->w;
+  //   h = s->h;
+  // }
 
-  uint32_t len = w * h;
-  uint32_t *buf = malloc(sizeof(uint32_t) * len);
-  uint32_t start_pos = x + y * s->w;
-  uint32_t i = 0;
-  for (size_t row = 0; row < h; ++row){
-    for (size_t col = 0; col < w; ++col){
-      uint32_t offset = col + row * s->w;
-      if (s->format->BitsPerPixel == 32){
-        s->pixels[start_pos + offset];
-        buf[i++] = s->pixels[start_pos + 4 * offset + 3] << 24 | s->pixels[start_pos + 4 * offset + 2] << 16 | s->pixels[start_pos + 4 * offset + 1] << 8 | s->pixels[start_pos + 4 * offset];
-      }
-      else if (s->format->BitsPerPixel == 8){
-        SDL_Color rgba_color = s->format->palette->colors[s->pixels[start_pos + offset]];
-        buf[i++] = rgba_color.a << 24 | rgba_color.r << 16 | rgba_color.g << 8 | rgba_color.b;
-      }
-      else
-        assert(0);    
+  // uint32_t len = w * h;
+  // uint32_t *buf = malloc(sizeof(uint32_t) * len);
+  // uint32_t start_pos = x + y * s->w;
+  // uint32_t i = 0;
+  // for (size_t row = 0; row < h; ++row){
+  //   for (size_t col = 0; col < w; ++col){
+  //     uint32_t offset = col + row * s->w;
+  //     if (s->format->BitsPerPixel == 32){
+  //       s->pixels[start_pos + offset];
+  //       buf[i++] = s->pixels[start_pos + 4 * offset + 3] << 24 | s->pixels[start_pos + 4 * offset + 2] << 16 | s->pixels[start_pos + 4 * offset + 1] << 8 | s->pixels[start_pos + 4 * offset];
+  //     }
+  //     else if (s->format->BitsPerPixel == 8){
+  //       SDL_Color rgba_color = s->format->palette->colors[s->pixels[start_pos + offset]];
+  //       buf[i++] = rgba_color.a << 24 | rgba_color.r << 16 | rgba_color.g << 8 | rgba_color.b;
+  //     }
+  //     else
+  //       assert(0);    
+  //   }
+  // }
+
+  // NDL_DrawRect(buf, x, y, w, h);
+
+  // free(buf);
+  if (s->format->BitsPerPixel == 32){
+    if (w == 0 && h == 0 && x ==0 && y == 0){
+      //printf("%d %d\n", s->w, s->h);
+      NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);
+      return ;
     }
+    
+    uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
+    assert(pixels);
+    uint32_t *src = (uint32_t *)s->pixels;
+    for (int i = 0; i < h; ++i){
+      memcpy(&pixels[i * w], &src[(y + i) * s->w + x], sizeof(uint32_t) * w);
+    }
+    NDL_DrawRect(pixels, x, y, w, h);
+
+    free(pixels);
+  }else if(s->format->BitsPerPixel == 8){
+    if (w == 0 && h == 0 && x ==0 && y == 0){
+      w = s->w; h = s->h;
+      x = 0;    y = 0;
+    }
+
+    uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
+    assert(pixels);
+    uint8_t *src = (uint8_t *)s->pixels;
+
+    for (int i = 0; i < h; ++i){
+      for (int j = 0; j < w; ++j){
+        pixels[i * w + j] = translate_color(&s->format->palette->colors[src[(y + i) * s->w + x + j]]);
+        //pixels[i * w + j] = s->format->palette->colors[src[(y + i) * s->w + x + j]].val;
+      }
+    }
+    NDL_DrawRect(pixels, x, y, w, h);
+
+    free(pixels);
+  }else {
+    assert(0);
   }
-
-  NDL_DrawRect(buf, x, y, w, h);
-
-  free(buf);
 }
 
 // APIs below are already implemented.
