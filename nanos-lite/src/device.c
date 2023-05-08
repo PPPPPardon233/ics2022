f3e942387341db57f603e4c3fc3716e40cbecd14
tracer-ics2022 <tracer@njuics.org>
2023-05-08 19:09:52 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:52 up  2:27,  1 user,  load average: 1.13, 0.67, 0.59

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -54,13 +54,14 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  uintptr_t *ptr;
-  ptr = (uintptr_t *)(&buf);
-
-  io_write(AM_GPU_MEMCPY, offset, (void *)*ptr, len);
-  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-  
-  return len;
+  uint32_t w = io_read(AM_GPU_CONFIG).width;
+	uint32_t h = io_read(AM_GPU_CONFIG).height;
+	uint32_t x = (offset / 4) % w; 
+	uint32_t y = (offset / 4) / w;
+	if (offset + len > w * h * 4)
+		len = w * h * 4 - offset;
+	io_write(AM_GPU_FBDRAW, x, y, (uint32_t *)buf, len / 4, 1, true);
+	return len;
 }
 
 void init_device() {
