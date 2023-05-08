44662a5b71c3a2a72e2142e823fff8c21ac026b8
tracer-ics2022 <tracer@njuics.org>
2023-05-08 18:14:37 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:14:37 up  1:32,  1 user,  load average: 0.35, 0.52, 0.50

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,8 +28,8 @@ void init_proc() {
   //naive_uload(NULL, "/bin/dummy");
   //naive_uload(NULL, "/bin/hello");
   //naive_uload(NULL, "/bin/file-test");
-  naive_uload(NULL, "/bin/timer-test");
-  //naive_uload(NULL, "/bin/event-test");
+  //naive_uload(NULL, "/bin/timer-test");
+  naive_uload(NULL, "/bin/event-test");
   //naive_uload(NULL, "/bin/bmp-test");
 }
 
