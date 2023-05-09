9d58605c0c9fc6642e6b4540f0c7ef2f769d0b54
tracer-ics2022 <tracer@njuics.org>
2023-05-09 10:45:55 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:55 up 12 min,  1 user,  load average: 0.42, 0.82, 0.76

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -72,7 +72,8 @@ int main() {
         case SDLK_DOWN: next(rep); rep = 0; g = 0; break;
         case SDLK_K:
         case SDLK_UP: prev(rep); rep = 0; g = 0; break;
-        case SDLK_G:
+        case SDLK_G:next(rep); rep = 0; g = 0; break;
+        default: 
           g ++;
           if (g > 1) {
             prev(100000);
--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -48,35 +48,6 @@ static int pop(uint8_t *type, uint8_t *sym){
 
 static uint8_t key_state[sizeof(keyname) / sizeof(keyname[0])] = {0};
 
-// void SDL_PumpEvents(void){
-//   assert(0);
-//   char buf[64], action[8], key[32];
-  
-//   //printf("Doing PumpEvent...\n");
-//   int ret;
-//   uint8_t type = 0, sym = 0;
-//   while ((ret = NDL_PollEvent(buf, sizeof(buf)), ret)){
-//     sscanf(buf, "%s %s", action, key);
-
-//     for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i){
-//       if (strcmp(key, keyname[i]) == 0){
-//         sym = i;
-//         break ;
-//       }
-//     }
-
-//     if (strcmp("kd", action) == 0){
-//       type = SDL_KEYDOWN;
-//     }else if(strcmp("ku", action) == 0){
-//       type = SDL_KEYUP;
-//     }else {
-//       assert(0);
-//     }
-
-//     append(type, sym);
-//   }
-// }
-
 
 int SDL_PushEvent(SDL_Event *ev) {
   assert(0);
