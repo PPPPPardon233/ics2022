5fcb81f9a0fa2884a4a5e8294465509e315da3c7
tracer-ics2022 <tracer@njuics.org>
2023-05-09 10:23:29 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:23:29 up 4 min,  1 user,  load average: 1.99, 2.88, 1.40

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -84,37 +84,36 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 static char key_buf[64], *key_action, *key_key;
-//To Be Fast
+
+#define _DEBUG_
+
 static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
   int ret = NDL_PollEvent(key_buf, sizeof(key_buf));
   if (!ret){
     return 0;
   }
-  //printf("%s\n", key_buf);
+  #ifdef _DEBUG_
+  printf("%s\n", key_buf);
+  #endif
   key_action = key_buf;
   int i;
   for (i = 0; key_buf[i] != ' '; i++){}
   key_buf[i] = '\0';
   key_key = &key_buf[i + 1]; 
-  
-  //截断\n
   for (i = 0;  key_key[i] != '\0' && key_key[i] != '\n'; i++){}
   if (key_key[i] == '\n'){
     key_key[i] = '\0';
   }
-  
-  //strcmp("kd", key_action) == 0
-  if (key_action[1] == 'd'){//加速！！
+  if (key_action[1] == '↓'){
     *type = SDL_KEYDOWN;
-  }else{
+  }
+  else{
     *type = SDL_KEYUP;
   }
 
   for (i = 0; i < sizeof(keyname) / sizeof(char *); ++i){
-    //剪枝掉很多
     if (key_key[0] == keyname[i][0] && strcmp(key_key, keyname[i]) == 0){
       *sym = i;
-      //printf("%d %d\n", *type, *sym);
       return ret;
     }
   }
@@ -122,11 +121,6 @@ static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
 
 int SDL_PollEvent(SDL_Event *ev) {
   uint8_t type = 0, sym = 0;
-  // SDL_PumpEvents();
-
-
-  // if (pop(&type, &sym)){
-  //printf("SDL_PollEvent\n");
   if (read_keyinfo(&type, &sym)){
     ev->type = type;
     ev->key.keysym.sym = sym;
@@ -134,29 +128,28 @@ int SDL_PollEvent(SDL_Event *ev) {
     switch(type){
     case SDL_KEYDOWN:
       key_state[sym] = 1;
-      //printf("%d Down\n", (int)sym);
+      #ifdef _DEBUG_
+      printf("%d Down\n", (int)sym);
+      #endif
       break;
     
     case SDL_KEYUP:
       key_state[sym] = 0;
-      //printf("%d Up\n", (int)sym);
+      #ifdef _DEBUG_
+      printf("%d Up\n", (int)sym);
+      #endif
       break;
     }
-  }else {
+  }
+  else {
     return 0;
   }
-
   return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
   uint8_t type = 0, sym = 0;
-  //SDL_PumpEvents();
-
-  //while (!pop(&type, &sym)){
-  while (!read_keyinfo(&type, &sym)){
-    //SDL_PumpEvents();
-  }
+  while (!read_keyinfo(&type, &sym)){}
   
   event->type = type;
   event->key.keysym.sym = sym;
@@ -170,30 +163,7 @@ int SDL_WaitEvent(SDL_Event *event) {
       key_state[sym] = 0;
       break;
   }
-
-  // char buf[64], action[8], key[32];
-  
-  // while (!NDL_PollEvent(buf, sizeof(buf))) {}
-  
-  // //注意：这里代码和Poll中重复
-  // sscanf(buf, "%s %s", action, key);
-  
-  // if (strcmp("kd", action) == 0){
-  //   event->type = SDL_KEYDOWN;
-  // }else if(strcmp("ku", action) == 0){
-  //   event->type = SDL_KEYUP;
-  // }else {
-  //   assert(0);
-  // }
-
-  // for (int i = 0; i < sizeof(keyname) / sizeof(char *); ++i){
-  //   if (strcmp(key, keyname[i]) == 0){
-  //     event->key.keysym.sym = i;
-  //     break ;
-  //   }
-  // }
-
-  // return 1;
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
