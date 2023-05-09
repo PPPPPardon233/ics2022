3063e3e724400ddc3b13c97baf775bb282eaa8d5
tracer-ics2022 <tracer@njuics.org>
2023-05-09 10:48:51 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:51 up 15 min,  1 user,  load average: 0.59, 0.83, 0.78

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -57,6 +57,7 @@ int main() {
     SDL_WaitEvent(&e);
 
     if (e.type == SDL_KEYDOWN) {
+      printf("down");
       switch(e.key.keysym.sym) {
         case SDLK_0: rep = rep * 10 + 0; break;
         case SDLK_1: rep = rep * 10 + 1; break;
