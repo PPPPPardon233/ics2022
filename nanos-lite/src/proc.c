8e0d9598df387f949c249320c978f000b439fc0c
tracer-ics2022 <tracer@njuics.org>
2023-05-09 13:40:57 +0800

    >  compile NEMU 2012009 lyz Linux pardon233-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:40:57 up  3:08,  1 user,  load average: 0.59, 0.35, 0.24

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,8 @@ void init_proc() {
   //naive_uload(NULL, "/bin/event-test");
   //naive_uload(NULL, "/bin/bmp-test");
   //naive_uload(NULL, "/bin/nslider");
-  naive_uload(NULL, "/bin/menu");
+  //naive_uload(NULL, "/bin/menu");
+  naive_uload(NULL, "/bin/nterm");
 }
 
 Context* schedule(Context *prev) {
