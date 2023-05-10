c9a4cff2eb80a7f6697506f3a821f13c92b56255
PPPPPardon233 <126453257+PPPPPardon233@users.noreply.github.com>
2023-05-10 16:21:25 +0800

    Update NDL.c

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -29,39 +29,39 @@ static int canvas_w, canvas_h, canvas_x = 0, canvas_y = 0;
 
 void NDL_OpenCanvas(int *w, int *h) {
   FILE* fp = fopen("/proc/dispinfo", "r");
-	fscanf(fp, "WIDTH : %d\nHEIGHT:%d", &screen_w, &screen_h);
-	if(*w == 0&&*h == 0){
-    		canvas_w = screen_w;
-    		canvas_h = screen_h;
-    		*w = screen_w;
-    		*h = screen_h;
-  	}
-  	else {
-		canvas_w = *w<=screen_w? *w:screen_w;		
-		canvas_h = *h<=screen_h? *h:screen_h;
-	}
-	canvas = (uint32_t*)malloc(sizeof(uint32_t)*(*w)*(*h));
-	memset(canvas , 0, sizeof(canvas));
-	offset_w = (screen_w - canvas_w)/2;
-	offset_h = (screen_h - canvas_h)/2;//居中
-
-  if (getenv("NWM_APP")) {
-    int fbctl = 4;
-    fbdev = 5;
-    screen_w = *w; screen_h = *h;
-    char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
-    // let NWM resize the window and create the frame buffer
-    write(fbctl, buf, len);
-    while (1) {
-      // 3 = evtdev
-      int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
-      buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
+    fscanf(fp, "WIDTH : %d\nHEIGHT:%d", &screen_w, &screen_h);
+    if(*w == 0&&*h == 0){
+	canvas_w = screen_w;
+	canvas_h = screen_h;
+	*w = screen_w;
+	*h = screen_h;
     }
-    close(fbctl);
-  }
+    else {
+	canvas_w = *w<=screen_w? *w:screen_w;		
+	canvas_h = *h<=screen_h? *h:screen_h;
+    }
+    canvas = (uint32_t*)malloc(sizeof(uint32_t)*(*w)*(*h));
+    memset(canvas , 0, sizeof(canvas));
+    offset_w = (screen_w - canvas_w)/2;
+    offset_h = (screen_h - canvas_h)/2;
+
+    if (getenv("NWM_APP")) {
+      int fbctl = 4;
+      fbdev = 5;
+      screen_w = *w; screen_h = *h;
+      char buf[64];
+      int len = sprintf(buf, "%d %d", screen_w, screen_h);
+      // let NWM resize the window and create the frame buffer
+      write(fbctl, buf, len);
+      while (1) {
+      // 3 = evtdev
+	int nread = read(3, buf, sizeof(buf) - 1);
+	if (nread <= 0) continue;
+	buf[nread] = '\0';
+	if (strcmp(buf, "mmap ok") == 0) break;
+      }
+      close(fbctl);
+    }
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
@@ -69,11 +69,11 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   if(w == 0||w > canvas_w)w = canvas_w;
   for(int i = 0;i < h;i ++)
     for(int j = 0;j < w;j ++)
-      canvas[(y+i)*canvas_w+(x+j)] = pixels[i*w+j];//canvas x,y处的图像
-    FILE* fp = fopen("/dev/fb","w");
-    for(int i = 0;i < canvas_h;i ++){
-    fseek(fp, 4*((i+offset_h)*screen_w+offset_w), SEEK_SET);
-    fwrite((void*)(canvas+i*canvas_w), 1, 4*canvas_w, fp);//按行输出到屏幕
+      canvas[(y+i)*canvas_w+(x+j)] = pixels[i*w+j];
+  FILE* fp = fopen("/dev/fb","w");
+  for(int i = 0;i < canvas_h;i ++){
+    fseek(fp, 4*((i+offset_h)*screen_w+offset_w), SEEK_SET)
+    fwrite((void*)(canvas+i*canvas_w), 1, 4*canvas_w, fp);
   }
 }
 
